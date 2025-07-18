bd9a9ab380e4faade942158e22a65335a731a159
Shkroba Daria <shkrobadaria@mail.ru>
2025-07-18 16:46:33 +0300

    add mock-data

---
+++ src/app/products.ts
@@ -0,0 +1,9 @@
+export const products = [
+  { id: '1', name: 'Товар 1', price: 1000, image: '/img1.jpg', description: 'Описание товара 1' },
+  { id: '2', name: 'Товар 2', price: 2000, image: '/img2.jpg', description: 'Описание товара 2' },
+  { id: '3', name: 'Товар 3', price: 3000, image: '/img3.jpg', description: 'Описание товара 3' },
+  { id: '4', name: 'Товар 4', price: 4000, image: '/img4.jpg', description: 'Описание товара 4' },
+  { id: '5', name: 'Товар 5', price: 5000, image: '/img5.jpg', description: 'Описание товара 5' },
+  { id: '6', name: 'Товар 6', price: 6000, image: '/img6.jpg', description: 'Описание товара 6' },
+
+]
